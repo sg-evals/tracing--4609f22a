ffb2a545c2d2c8c46e0561ea7b3f22745c762f71
sg-evals <benchmarks@sourcegraph.com>
2026-03-02 18:23:50 +0000

    Mirror of tokio-rs/tracing at 4609f22aff1ad88b81e749e2536761d6ee364d1f
    Upstream: https://github.com/tokio-rs/tracing
    Ref: 4609f22aff1ad88b81e749e2536761d6ee364d1f

---
+++ examples/examples/opentelemetry.rs
@@ -0,0 +1,38 @@
+use std::{error::Error, thread, time::Duration};
+use tracing::{span, trace, warn};
+use tracing_attributes::instrument;
+use tracing_subscriber::prelude::*;
+
+#[instrument]
+#[inline]
+fn expensive_work() -> &'static str {
+    span!(tracing::Level::INFO, "expensive_step_1")
+        .in_scope(|| thread::sleep(Duration::from_millis(25)));
+    span!(tracing::Level::INFO, "expensive_step_2")
+        .in_scope(|| thread::sleep(Duration::from_millis(25)));
+
+    "success"
+}
+
+fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
+    let (tracer, _uninstall) = opentelemetry_jaeger::new_pipeline()
+        .with_service_name("report_example")
+        .install()?;
+    let opentelemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+    tracing_subscriber::registry()
+        .with(opentelemetry)
+        .try_init()?;
+
+    let root = span!(tracing::Level::INFO, "app_start", work_units = 2);
+    let _enter = root.enter();
+
+    let work_result = expensive_work();
+
+    span!(tracing::Level::INFO, "faster_work")
+        .in_scope(|| thread::sleep(Duration::from_millis(10)));
+
+    warn!("About to exit!");
+    trace!("status: {}", work_result);
+
+    Ok(())
+}
